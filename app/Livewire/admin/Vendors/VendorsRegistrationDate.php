aaefdb15000c6341e4580018e5af385ce6509c46
Ahmed Alaa <ahmed.alaaeldine21@gmail.com>
2024-10-20 20:47:27 +0300

    add validation roles after vendor_manager make save

--- app/Livewire/admin/Vendors/VendorsRegistrationDate.php
+++ app/Livewire/admin/Vendors/VendorsRegistrationDate.php
@@ -6,6 +6,19 @@ use Livewire\Component;
 
 class VendorsRegistrationDate extends Component
 {
+    public $start_at, $end_at;
+    public function roles()
+    {
+        return [
+            'start_at' => 'required|date',
+            'end_at' => 'required|date|after:start_at',
+        ];
+    }
+    public function submit()
+    {
+        $this->validate($this->roles());
+        
+    }
     public function render()
     {
         return view('admin.vendors.vendors-registration-date');
--- resources/views/admin/vendors/vendors-registration-date.blade.php
+++ resources/views/admin/vendors/vendors-registration-date.blade.php
@@ -1,22 +1,25 @@
 <div class="col-xl-6">
-    <!-- HTML5 Inputs -->
-    <div class="card mb-4">
-        <h5 class="card-header">Vendor Registration Date</h5>
-        <div class="card-body">
-            <div class="mb-3 row">
-                <label for="html5-datetime-local-input" class="col-md-2 col-form-label">Start At:</label>
-                <div class="col-md-10">
-                    <input class="form-control" type="datetime-local" value="2021-06-18T12:30:00"
-                        id="html5-datetime-local-input" />
+    <form method="post" wire:submit.prevent='submit'>
+        @csrf
+        <div class="card mb-4">
+            <h5 class="card-header">Vendor Registration Date</h5>
+            <div class="card-body">
+                <div class="mb-3 row">
+                    <label for="start_at" class="col-md-2 col-form-label">Start At:</label>
+                    <div class="col-md-10">
+                        <input class="form-control" type="datetime-local" id="start_at" wire:model='start_at' />
+                        @include('admin.error', ['property' =>'start_at'])
+                    </div>
                 </div>
-            </div>
-            <div class="mb-3 row">
-                <label for="html5-datetime-local-input" class="col-md-2 col-form-label">End At:</label>
-                <div class="col-md-10">
-                    <input class="form-control" type="datetime-local" value="2021-06-18T12:30:00"
-                        id="html5-datetime-local-input" />
+                <div class="mb-3 row">
+                    <label for="end_at" class="col-md-2 col-form-label">End At:</label>
+                    <div class="col-md-10">
+                        <input class="form-control" type="datetime-local" id="end_at" wire:model='end_at' />
+                        @include('admin.error', ['property' =>'end_at'])
+                    </div>
                 </div>
+                <button class="btn btn-primary" wire:click='submit'>Save</button>
             </div>
         </div>
-    </div>
+    </form>
 </div>
