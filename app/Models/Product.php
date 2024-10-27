da5327a325fd03602bdc8970cbfbf6c3e72daf79
Ahmed Alaa <ahmed.alaaeldine21@gmail.com>
2024-10-27 08:48:17 +0300

    add product discount model and migration

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -5,26 +5,33 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Vendor;
+
 class Product extends Model
 {
     use HasFactory;
-    protected $fillable = ['title', 'description', 'price', 'total', 'quantity','slug','vendor_id'];
+    protected $fillable = ['title', 'description', 'price', 'total', 'quantity', 'slug', 'vendor_id'];
     public function vendor()
     {
         return $this->belongsTo(Vendor::class);
     }
     public function categories()
     {
-        return $this->belongsToMany(Category::class,'category_product','product_id','category_id');
+        return $this->belongsToMany(Category::class, 'category_product', 'product_id', 'category_id');
     }
-    public function getCategoryName(){
+    public function getCategoryName()
+    {
         return $this->categories->pluck('name')->first();
     }
     public function photos()
     {
         return $this->hasMany(ProductPhotos::class);
     }
-    public function cart(){
+    public function cart()
+    {
         return $this->belongsTo(Cart::class);
+    }
+    public function discount()
+    {
+        return $this->hasOne(Product::class, 'product_slug', 'slug');
     }
 }
