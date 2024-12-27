f5e3cd0dbc05d3e8f26c119be50c380cf4f950ff
Ahmed Alaa <ahmed.alaaeldine21@gmail.com>
2024-12-27 18:23:15 +0200

    add soft delete and restore to sizes

--- app/Livewire/admin/Sizes/SizesData.php
+++ app/Livewire/admin/Sizes/SizesData.php
@@ -7,17 +7,35 @@ use Livewire\Component;
 
 class SizesData extends Component
 {
-    public $search;
-    protected $listeners = ['refreshSizes' => '$refresh'];
+    public $search,$showArchived=false;
+    protected $listeners = ['refreshSizes' => '$refresh','restoreSize'];
     public function UpdateingSearch(){
         $this->resetPage();
+    }
+    public function showArchivedSizes()
+    {
+        if ($this->showArchived) {
+            $this->showArchived = false;
+        } else {
+            $this->showArchived = true;
+        }
+    }
+    public function restoreSize($id)
+    {
+        Size::where('id', $id)->restore();
     }
     public function deleteSize($id){
         dd($id);
     }
     public function render()
     {
-        $sizes = Size::where('name', 'like', '%' . $this->search . '%')->paginate(10);
+        $query = $this->showArchived
+            ? Size::onlyTrashed()
+            : Size::query();
+
+        $sizes = $query
+            ->where('name', 'like', '%' . $this->search . '%')
+            ->paginate(10);
         return view('admin.sizes.sizes-data', compact('sizes'));
     }
 }
