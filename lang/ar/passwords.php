d9a771f0bbcc1a01172c280049677515daf3dc2b
Ahmed Alaa <ahmed.alaaeldine21@gmail.com>
2024-12-29 11:05:15 +0200

    add arabic language file and translate some words

---
+++ lang/ar/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'لقد تم ضبط الباسورد الخاص بك',
+    'sent' => 'لقد تم ارسال الرابط علي الايميل الخاص بك',
+    'throttled' => 'الرجاء الانتظار قليلا قبل المحاولة',
+    'token' => 'This password reset token is invalid.',
+    'user' => "الايميل اللذي ادخلته غير موجود",
+
+];
